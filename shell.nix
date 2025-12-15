6540cba7e46c13f09406621233a7c9b9da2fb77e
Taeer Bar-Yam <taeer@bar-yam.me>
2025-12-15 21:11:32 +0100

    pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,5 +1,10 @@
 let
-  pkgs = import <nixpkgs> {};
+  nixpkgs = builtins.fetchTarball {
+    name = "nixos-25.11";
+    url = "https://github.com/nixos/nixpkgs/archive/c8cfcd6ccd422e41cc631a0b73ed4d5a925c393d.tar.gz";
+    sha256 = "sha256:1fdjh5jd5jx246fhfq13q5752nw9il5dwv36nqbcj4pa6kzwq9fy";
+  };
+  pkgs = import nixpkgs {};
 in
   pkgs.mkShell {
     packages = with pkgs; [
