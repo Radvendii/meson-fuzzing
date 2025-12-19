bce99a96b4a480991cb27f2075ac5b3f001f32eb
Taeer Bar-Yam <taeer@bar-yam.me>
2025-12-19 16:28:23 +0100

    "fix" meson with dumb patch

--- shell.nix
+++ shell.nix
@@ -8,7 +8,28 @@ let
 in
   pkgs.mkShell {
     packages = with pkgs; [
-      meson
+      (meson.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [(
+          pkgs.writeText "dont-check-sanitizers.patch" ''
+            diff --git i/mesonbuild/compilers/compilers.py w/mesonbuild/compilers/compilers.py
+            index a823aeb00..7209e853a 100644
+            --- i/mesonbuild/compilers/compilers.py
+            +++ w/mesonbuild/compilers/compilers.py
+            @@ -362,11 +362,7 @@ def get_base_link_args(target: 'BuildTarget',
+                     if sanitizer == ['none']:
+                         sanitizer = []
+                     sanitizer_args = linker.sanitizer_link_args(sanitizer)
+            -        # We consider that if there are no sanitizer arguments returned, then
+            -        # the language doesn't support them.
+                     if sanitizer_args:
+            -            if not linker.has_multi_link_arguments(sanitizer_args, env)[0]:
+            -                raise MesonException(f'Linker {linker.name_string()} does not support sanitizer arguments {sanitizer_args}')
+                         args.extend(sanitizer_args)
+                 except KeyError:
+                     pass
+          ''
+          )];
+      }))
       clang
       ninja
     ];
